dd7e646686b1a6d6dcc9a2dd1f40987de71a7a36
Cyprien83 <cyprien.toucas@ynov.com>
2017-10-20 14:58:52 +0200

    Delete logoutform.inc.php

--- views/templates/logoutform.inc.php
+++
@@ -1,7 +0,0 @@
-<form class="navbar-form pull-right">
-	<?php if ($this->login!=null)  $this->displayCommands(); ?>	
-	<a href="<?php echo $_SERVER['PHP_SELF']; ?>?action=Logout" class="btn btn-danger" >Déconnexion</a>
-	<span class="btn disabled"><?php echo $this->login; ?></span> 
-</form>
-
-
