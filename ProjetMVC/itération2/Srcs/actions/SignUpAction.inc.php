981a20595cda9d87da4bf0772c766c78d5e31567
Cyprien83 <cyprien.toucas@ynov.com>
2017-10-22 20:52:21 +0100

    Add files via upload

--- ProjetMVC/itération2/Srcs/actions/SignUpAction.inc.php
+++ ProjetMVC/itération2/Srcs/actions/SignUpAction.inc.php
@@ -2,35 +2,57 @@
 
 require_once("actions/Action.inc.php");
 
-class SignUpAction extends Action {
 
-	/**
-	 * Traite les données envoyées par le formulaire d'inscription
-	 * ($_POST['signUpLogin'], $_POST['signUpPassword'], $_POST['signUpPassword2']).
-	 *
-	 * Le compte est crée à l'aide de la méthode 'addUser' de la classe Database.
-	 *
-	 * Si la fonction 'addUser' retourne une erreur ou si le mot de passe et sa confirmation
-	 * sont différents, on envoie l'utilisateur vers la vue 'SignUpForm' contenant 
-	 * le message retourné par 'addUser' ou la chaîne "Le mot de passe et sa confirmation 
-	 * sont différents.";
-	 *
-	 * Si l'inscription est validée, le visiteur est envoyé vers la vue 'MessageView' avec
-	 * un message confirmant son inscription.
-	 *
-	 * @see Action::run()
-	 */
-	public function run() {
-		/* TODO START */
-		/* TODO END */
-	}
+class SignUpAction extends Action
+{
 
-	private function setSignUpFormView($message) {
-		$this->setView(getViewByName("SignUpForm"));
-		$this->getView()->setMessage($message);
-	}
+    /**
+     * Traite les données envoyées par le formulaire d'inscription
+     * ($_POST['signUpLogin'], $_POST['signUpPassword'], $_POST['signUpPassword2']).
+     *
+     * Le compte est crée à l'aide de la méthode 'addUser' de la classe Database.
+     *
+     * Si la fonction 'addUser' retourne une erreur ou si le mot de passe et sa confirmation
+     * sont différents, on envoie l'utilisateur vers la vue 'SignUpForm' contenant
+     * le message retourné par 'addUser' ou la chaîne "Le mot de passe et sa confirmation
+     * sont différents.";
+     *
+     * Si l'inscription est validée, le visiteur est envoyé vers la vue 'MessageView' avec
+     * un message confirmant son inscription.
+     *
+     * @see Action::run()
+     */
+    public function run()
+    {
+
+        $login       = $_POST['signUpLogin'];
+        $pass        = $_POST['signUpPassword'];
+        $passConfirm = $_POST['signUpPassword2'];
+
+
+        # Check password
+        if ($pass === $passConfirm) {
+
+            if ($this->database->addUser($login, $pass) === true) {
+                $this->setView(getViewByName('Message'));
+                $this->getView()->setMessage('Votre compte à été crée avec succès.');
+            } else {
+                $this->setSignUpFormView('Erreur.');
+            }
+        } else {
+            # Set message password don't mach
+            $this->setSignUpFormView('Le mot de passe et sa confirmation sont différents.');
+        }
+
+    }
+
+    /**
+     * @param $message
+     */
+    private function setSignUpFormView($message)
+    {
+        $this->setView(getViewByName("SignUpForm"));
+        $this->getView()->setMessage($message);
+    }
 
 }
-
-
-?>
